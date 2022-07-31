27a3fc453eb899959f8ec8e1688f8ca9a440ddec
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2022-07-31 13:43:37 +0900

    Fragmentでレポジトリ表示

--- app/src/main/java/com/example/github_api_sample/github/app/view/MainActivity.kt
+++ app/src/main/java/com/example/github_api_sample/github/app/view/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.github_api_sample.R
 import com.example.github_api_sample.databinding.ActivityMainBinding
 import com.example.github_api_sample.github.app.viewmodel.GitHubViewModel
@@ -13,18 +14,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
+import okhttp3.internal.notify
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private val viewModel = GitHubViewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setupBinding()
         setupButton()
-        observeLiveData()
-        viewModel.onCreate()
     }
 
     private fun setupBinding() {
@@ -32,20 +31,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupButton() {
-        binding.btnRepositories.setOnClickListener { viewModel.onFetchRepositories() }
+        binding.btnRepositories.setOnClickListener {
+            supportFragmentManager.beginTransaction()
+                .addToBackStack(BACK_STACK_TAG)
+                .add(R.id.fragmentContainerView, GitHubRepositoriesFragment.newInstance())
+                .commit()
+        }
     }
 
-    private fun observeLiveData() {
-        viewModel.userData.observe(this) {
-            binding.tvName.text = it.name
-        }
-
-        viewModel.repositories.observe(this) {
-            it.forEach { repository ->
-                Log.d(
-                    "Tatsuyað", "repository: ${repository}"
-                )
-            }
-        }
+    companion object {
+        private const val BACK_STACK_TAG = "back_stack_tag"
     }
 }
